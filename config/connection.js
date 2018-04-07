e7737c6aad64a11b3dad02a8467a3895bad23118
Slickrick88 <rickablake@gmail.com>
2018-04-07 17:06:58 -0400

    jaws db deployment

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+var connection;
+
+if (process.env.JAWSDB_url) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    // port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burger_db"
+  });
+};
+
+
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
